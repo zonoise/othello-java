53b0a4bb517ad8c345c83d6b965a8de74169cdf8
zonoise <zonoise@gmail.com>
2016-02-19 17:33:35 +0900

    add GameTest

---
+++ src/test/java/com/example/game/GameTest.java
@@ -0,0 +1,16 @@
+package com.example.game;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by zonoise on 2016/02/19.
+ */
+public class GameTest extends TestCase
+{
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+
+
+}
