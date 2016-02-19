ceb9b608cd214de6036e87b8502279cbcd4c6a50
zonoise <zonoise@gmail.com>
2016-02-19 19:25:34 +0900

    PutCommandTest

--- src/test/java/com/example/model/command/PutCommandTest.java
+++ src/test/java/com/example/model/command/PutCommandTest.java
@@ -14,7 +14,6 @@ public class PutCommandTest extends TestCase {
         super.setUp();
     }
 
-
     /*
 0 0 0 0 0 0 0 0
 0 0 0 0 0 0 0 0
@@ -26,45 +25,69 @@ public class PutCommandTest extends TestCase {
 0 0 0 0 0 0 0 0
  */
     public Game put(int x,int y){
+    try {
         Board board = new Board();
         board.init();
         Game game = new Game(board);
+        game.setStatus(Game.STATUS_BLACK);
         game.run(new InitializeCommand());
+        //return game;
+        Point point = new Point(x, y);
+        PutCommand p = new PutCommand(point);
+        game.run(p);
+        //p.execute(game);
         return game;
-//        Point point = new Point(x,y);
-//        PutCommand p = new PutCommand(point);
-//        game.run(p);
-//        //p.execute(game);
-//        return game;
+}catch (Exception e){
+    return null;
+}
     }
 
     public void test1(){
         Game game = put(2,3);
         String after = game.getBoard().toDetailString();
-        //System.out.println(after);
+        System.out.println(after);
         String expect =
-        "0 0 0 0 0 0 0 0 \n" +
-        "0 0 0 0 0 0 0 0 \n" +
-        "0 0 0 0 0 0 0 0 \n" +
-        "0 0 0 W B 0 0 0 \n" +
-        "0 0 0 B W 0 0 0 \n" +
-        "0 0 0 0 0 0 0 0 \n" +
-        "0 0 0 0 0 0 0 0 \n" +
-        "0 0 0 0 0 0 0 0 \n";
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 B B B 0 0 0 \n" +
+            "0 0 0 B W 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n";
         assertEquals(expect,after);
     }
 
-    public void test2(){
-        int x = 2;
-        int y = 3;
-        Board board = new Board();
-        board.init();
-        Game game = new Game(board);
-        game.run(new InitializeCommand());
-        Point point = new Point(x,y);
-        PutCommand p = new PutCommand(point);
-        game.run(p);
-        //p.execute(game);
-        //return game;
+    public void test2() throws Exception{
+        Game game = put(3,2);
+        String after = game.getBoard().toDetailString();
+        System.out.println(after);
+        String expect =
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 B 0 0 0 0 \n" +
+            "0 0 0 B B 0 0 0 \n" +
+            "0 0 0 B W 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n";
+        assertEquals(expect,after);
+    }
+
+
+    public void test3() throws Exception{
+        Game game = put(5,4);
+        String after = game.getBoard().toDetailString();
+        System.out.println(after);
+        String expect =
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 W B 0 0 0 \n" +
+            "0 0 0 B B B 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n" +
+            "0 0 0 0 0 0 0 0 \n";
+        assertEquals(expect,after);
     }
 }
