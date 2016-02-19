7e8765cf0da5dfd4fe930f7be8ea6a1138913a93
zonoise <zonoise@gmail.com>
2016-02-19 18:00:22 +0900

    comment out

--- src/test/java/com/example/game/GameTest.java
+++ src/test/java/com/example/game/GameTest.java
@@ -1,16 +1,15 @@
-package com.example.game;
-
-import junit.framework.TestCase;
-
-/**
- * Created by zonoise on 2016/02/19.
- */
-public class GameTest extends TestCase
-{
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-
-
-
-}
+//package com.example.game;
+//
+//import junit.framework.TestCase;
+//
+///**
+// * Created by zonoise on 2016/02/19.
+// */
+//public class GameTest extends TestCase
+//{
+//    public void setUp() throws Exception {
+//        super.setUp();
+//    }
+//
+//
+//}
