65384001429be8873f38cd956c23b9f9834a318c
zonoise <zonoise@gmail.com>
2016-02-24 16:43:14 +0900

    GameEnd

---
+++ src/main/java/com/example/model/result/EndResult.java
@@ -0,0 +1,11 @@
+package com.example.model.result;
+
+/**
+ * Created by zonoise on 2016/02/24.
+ */
+public class EndResult extends CommandResult {
+    @Override
+    public int getType() {
+        return this.END;
+    }
+}
